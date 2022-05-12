0889214e9e8e5abff90ac7ebb522e2c4a6ad638b
Peter Parker <web3slinger@gmail.com>
2022-05-12 09:22:27 -0500

    Build contract

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = '0xFA72242a74d4c874336D0ee61acd15909B56d2c8'
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS }
