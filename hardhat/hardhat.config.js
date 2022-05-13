9e6641d1532f5eb6d2fca1896e5171a8adf1f660
Peter Parker <web3slinger@gmail.com>
2022-05-13 08:11:25 -0500

    Update solidity version

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,7 @@ const ALCHEMY_RINKEBY_URL = process.env.ALCHEMY_RINKEBY_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
-  solidity: '0.8.10',
+  solidity: '0.8.4',
   networks: {
     rinkeby: {
       url: ALCHEMY_RINKEBY_URL,
